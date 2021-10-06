fdeb6a50293065f580b5eef313c9c0938ac082fe
MontesRamos <ruben.eduardo.tec@gmail.com>
2021-10-06 18:09:06 -0500

    remove circular module ref

--- module_config.tf
+++
@@ -1,8 +0,0 @@
-module "terraform-aws-vpc-rm" {
-  source = "github.com/MontesRamos/terraform-vpc-module-github-1?ref=v1.0.0"
-  vpc_name = var.vpc_name
-  cidr_block = var.cidr_block
-  subnet_name = var.subnet_name
-  region = var.region
-  profile = var.profile
-}
